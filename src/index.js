24bfa6865802c61503d279c3fc7bcb4b24374542
bcdguz <bcdguz96@gmail.com>
2021-03-01 17:56:27 -0800

    Setting up canvas element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import _ from 'lodash';
+import Game from './components/game';
 
-function component() {
-    const element = document.createElement('div');
-
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+window.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById('bird-game');
+    const game = new Game(canvas);
+})
