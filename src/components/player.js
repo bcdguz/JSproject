4f9e48166fa17edaa31e6b8a11d4c41a4f83c440
bcdguz <bcdguz96@gmail.com>
2021-03-02 14:52:29 -0800

    Adding #move to player class

--- src/components/player.js
+++ src/components/player.js
@@ -1,10 +1,17 @@
+const CONSTANTS = {
+    PLAYER_SPEED: 0.4
+}
+
+
 export default class Player {
     constructor(dimensions){
-        this.playerPosY = dimensions.height/3;
-        this.playerPosX = dimensions.width/3;
+        this.playerPosY = dimensions.height/2;
+        this.playerPosX = dimensions.width/5;
+        this.movement = "";
     }
 
     animate(ctx){
+        this.move();
         this.drawPlayer(ctx);
     }
 
@@ -14,5 +21,18 @@ export default class Player {
         ctx.fillStyle = "brown";
         ctx.fill()
         ctx.stroke();
+    }
+
+    move(){
+        switch(this.movement) {
+            case "LEFT":
+                this.playerPosX -= CONSTANTS.PLAYER_SPEED;
+            case "RIGHT":
+                this.playerPosX += CONSTANTS.PLAYER_SPEED;
+            case "UP":
+                this.playerPosY -= CONSTANTS.PLAYER_SPEED;
+            case "DOWN":
+                this.playerPosY += CONSTANTS.PLAYER_SPEED;
+        }
     }
 }
