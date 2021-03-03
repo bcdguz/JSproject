0fa2785c33614443ca9bf8ac43ddcf42f9fa37c2
bcdguz <bcdguz96@gmail.com>
2021-03-03 10:04:37 -0800

    Finishing map boundary for player

--- src/components/player.js
+++ src/components/player.js
@@ -49,49 +49,68 @@ export default class Player {
         }
     }
 
-
-    move(){
-        let move = this.movement;
-        const speed = CONSTANTS.PLAYER_SPEED;
-        const radius = CONSTANTS.PLAYER_RADIUS;
-        const dimensions = this.dimensions;
-
-        //boundary checks
-        const upBound = this.playerPosY - speed - radius > 0;
-        const lowBound = this.playerPosY + speed + radius < dimensions.height;
-        const leftBound = this.playerPosX - speed - radius > 0;
-        const rightBound = this.playerPosX + speed + radius < dimensions.width;
-        
-        if (move["wKey"] && move["aKey"]) {
-            if (upBound && leftBound) {
-                this.playerPosX -= 0.5 * speed;
-                this.playerPosY -= 0.5 * speed;
-            }
-        } else if (move["wKey"] && move["dKey"]) {
-            this.playerPosX += 0.5 * speed;
-            this.playerPosY -= 0.5 * speed;
-        } else if (move["sKey"] && move["aKey"]) {
-            this.playerPosX -= 0.5 * speed;;
-            this.playerPosY += 0.5 * speed;
-        } else if (move["sKey"] && move["dKey"]) {
-            this.playerPosX += 0.5 * speed;
-            this.playerPosY += 0.5 * speed;
-        } else if (move["wKey"]) {
-            if (upBound) this.playerPosY -= speed;
-        } else if (move["sKey"]) {
-            if (lowBound) this.playerPosY += speed;
-        } else if (move["aKey"]) {
-            if (leftBound) this.playerPosX -= speed;
-        } else if (move["dKey"]) {
-            if (rightBound) this.playerPosX += speed;
-        }
-    }
-
-    look(dir){
+    look(dir) {
         // This method takes in xy of mouse pos to return angle
         //x,y relative to players position
         let dy = dir.y - this.playerPosY;
         let dx = dir.x - this.playerPosX;
         this.angle = Math.atan2(dy, dx);
     }
+
+
+    move(){
+        let move = this.movement;
+        const speed = CONSTANTS.PLAYER_SPEED;
+        const radius = CONSTANTS.PLAYER_RADIUS;
+        const height = this.dimensions.height;
+        const width = this.dimensions.width;
+
+        //boundary checks
+        //non boolean checks
+        const upBound = this.playerPosY - speed - radius;
+        const lowBound = this.playerPosY + speed + radius;
+        const leftBound = this.playerPosX - speed - radius;
+        const rightBound = this.playerPosX + speed + radius;
+
+        //boolean checks
+        const upLeftBound = upBound + 0.5 * speed > 0 &&
+                            leftBound - 0.5 * speed > 0;
+        const upRightBound = upBound + 0.5 * speed > 0 &&
+                            rightBound - 0.5 * speed < width;
+        const lowLeftBound = lowBound - 0.5 * speed < height &&
+                            leftBound - 0.5 * speed > 0;
+        const lowRightBound = lowBound - 0.5 * speed < height &&
+                            rightBound - 0.5 * speed < width;
+
+        if (move["wKey"] && move["aKey"]) {
+            if (upLeftBound) {
+                this.playerPosX -= 0.5 * speed;
+                this.playerPosY -= 0.5 * speed;
+            }
+        } else if (move["wKey"] && move["dKey"]) {
+            if (upRightBound) {
+                this.playerPosX += 0.5 * speed;
+                this.playerPosY -= 0.5 * speed;
+            }
+        } else if (move["sKey"] && move["aKey"]) {
+            if (lowLeftBound) {
+                this.playerPosX -= 0.5 * speed;;
+                this.playerPosY += 0.5 * speed;
+            }
+        } else if (move["sKey"] && move["dKey"]) {
+            if (lowRightBound) {
+                this.playerPosX += 0.5 * speed;
+                this.playerPosY += 0.5 * speed;
+            }
+        } else if (move["wKey"]) {
+            if (upBound > 0) this.playerPosY -= speed;
+        } else if (move["sKey"]) {
+            if (lowBound < height) this.playerPosY += speed;
+        } else if (move["aKey"]) {
+            if (leftBound > 0) this.playerPosX -= speed;
+        } else if (move["dKey"]) {
+            if (rightBound < width) this.playerPosX += speed;
+        }
+    }
+
 }
