f23ac3f03133b4e4ace360304b4273039522f1c3
bcdguz <bcdguz96@gmail.com>
2021-03-03 12:20:13 -0800

    Creating #collidesWith in map class

--- src/components/map.js
+++ src/components/map.js
@@ -2,7 +2,7 @@ import { walls } from './util/constants';
 
 export default class Map {
     constructor(dimensions){
-        this.dimensions = dimensions;
+        this.dimensions = dimensions; //canvas dimensions
         this.wallImg = document.getElementById('wall');
         this.background = document.getElementById('background');
         this.walls = walls;
@@ -18,10 +18,34 @@ export default class Map {
         this.dimensions.width, this.dimensions.height);    
     }
 
-    drawWalls(ctx) {
-        this.walls.forEach(wall => {
+    drawWalls(ctx){
+        this.eachWall(wall => {
             ctx.drawImage(this.wallImg, wall.posX,
             wall.posY, wall.width, wall.height);
         });
+    }
+
+    eachWall(cb){ //preserve my walls context
+        this.walls.forEach(cb.bind(this));
+    }
+
+    collidesWith(player){
+        this.eachWall(wall => {
+            const wallLeft = wall.posX;
+            const wallRight = wall.posX + wall.width;
+            const wallTop = wall.posY;
+            const wallBot = wall.posY + wall.height;
+        })
+        
+        if (obj1.left > obj2.right || obj1.right < obj2.left) {
+            return false;
+        }
+        if (obj1.top > obj2.bottom || obj1.bottom < obj2.top) {
+            return false;
+        }
+        return true;
+        
+
+
     }
 }
