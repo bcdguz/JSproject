b93ebadd91ae65f44d7e7baba9d98e8d74c8c538
bcdguz <bcdguz96@gmail.com>
2021-03-07 20:19:57 -0800

    Un-nesting index.html and installing bundle-js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,22 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname)
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
+        ],
     },
 };
